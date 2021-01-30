861ec70d0238c6390296112fd999d1203dec5216
Aleksandr <alexandrgaiko88@gmail.com>
2021-01-30 21:57:14 +0100

    add pagination

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -17,24 +17,24 @@ class ProductController extends Controller
     public function showCategory(Request $request, $cat_alias) {
     	$cat = Category::where('alias', $cat_alias)->first();
 
-    	$products = Product::where('category_id', $cat->id)->get();
-
+    	$pagination = 2;
+    	$products = Product::where('category_id', $cat->id)->paginate($pagination);
     		if(isset($request->orderBy)){
 
     			if($request->orderBy == 'price-low-high'){
-    			$products = Product::where('category_id', $cat->id)->orderBy('price')->get();
+    			$products = Product::where('category_id', $cat->id)->orderBy('price')->paginate($pagination);
     			}
 
     			if($request->orderBy == 'price-high-low'){
-    			$products = Product::where('category_id', $cat->id)->orderBy('price', 'desc')->get();
+    			$products = Product::where('category_id', $cat->id)->orderBy('price', 'desc')->paginate($pagination);
     			}
 
     			if($request->orderBy == 'name-a-z'){
-    			$products = Product::where('category_id', $cat->id)->orderBy('title')->get();
+    			$products = Product::where('category_id', $cat->id)->orderBy('title')->paginate($pagination);
     			}
 
     			if($request->orderBy == 'name-z-a'){
-    			$products = Product::where('category_id', $cat->id)->orderBy('title', 'desc')->get();
+    			$products = Product::where('category_id', $cat->id)->orderBy('title', 'desc')->paginate($pagination);
     			}
     		}
     	
